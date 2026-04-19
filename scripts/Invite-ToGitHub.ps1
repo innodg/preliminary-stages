607d4a007ec6e647dfcd0a948f6c1fdf64facef3
Justin Yoo <justin.yoojh@gmail.com>
2026-04-20 00:58:31 +0900

    Update GHA workflows

--- scripts/Invite-ToGitHub.ps1
+++ scripts/Invite-ToGitHub.ps1
@@ -3,8 +3,12 @@ Param(
     [string] $GitHubOrg,
 
     [Parameter(Mandatory = $true)]
-    [string] $GitHubId
+    [string] $GitHubId,
+
+    [Parameter(Mandatory = $true)]
+    [string] $TeamSlug
 )
 
-$userId = $(gh api users/$GitHubId --jq '.id')
-gh api --method POST /orgs/$GitHubOrg/invitations -F invitee_id=$userId
+$userId = $(gh api /users/$GitHubId --jq '.id')
+$teamId = $(gh api /orgs/$GitHubOrg/teams/$TeamSlug --jq '.id')
+gh api --method POST /orgs/$GitHubOrg/invitations -F invitee_id=$userId -F "team_ids[]=$teamId"
